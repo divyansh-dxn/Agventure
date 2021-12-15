6e0954a99ea623794748a31fcd4c8830759026ac
DeepanshuPratik <deepanshu.pratik@gmail.com>
2021-12-16 01:32:38 +0530

    Seller Layout

---
+++ android/seller/src/main/java/com/dxn/seller/recyclerDataFormat.kt
@@ -0,0 +1,5 @@
+package com.dxn.seller
+
+import android.widget.ImageView
+
+class RecyclerData(var productImage: ImageView, var productName: String,var productPrice: String,var productQuantity: Int)
