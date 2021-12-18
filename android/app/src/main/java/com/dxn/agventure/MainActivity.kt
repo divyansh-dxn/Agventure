ba93b0118ef3ba0a03c12d7cb3b994e605ee2564
Divyansh Kushwaha <kushwaha.divyansh.dxn@gmail.com>
2021-12-18 18:33:35 +0530

    basic setup

--- android/app/src/main/java/com/dxn/agventure/MainActivity.kt
+++ android/app/src/main/java/com/dxn/agventure/MainActivity.kt
@@ -2,10 +2,15 @@ package com.dxn.agventure
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
