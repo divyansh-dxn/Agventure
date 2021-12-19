16e171f698c0068f33e6571e0f07bcb122e0ea72
Divyansh Kushwaha <kushwaha.divyansh.dxn@gmail.com>
2021-12-19 09:59:08 +0530

    basic setup

--- android/auth/src/main/java/com/dxn/auth/Auth.kt
+++ android/auth/src/main/java/com/dxn/auth/Auth.kt
@@ -2,19 +2,24 @@ package com.dxn.auth
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.dxn.auth.databinding.FragmentAuthBinding
-import com.dxn.auth.databinding.FragmentOnBoardingBinding
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.*
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import java.util.concurrent.TimeUnit
 
 class Auth : Fragment() {
@@ -23,6 +28,7 @@ class Auth : Fragment() {
     private var _binding: FragmentAuthBinding? = null
     private val binding get() = _binding!!
     private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,69 +37,62 @@ class Auth : Fragment() {
         _binding = FragmentAuthBinding.inflate(inflater, container, false)
         FirebaseApp.initializeApp(requireContext())
         auth = Firebase.auth
+        firestore = FirebaseFirestore.getInstance()
         setupCallbacks()
         val options = PhoneAuthOptions.newBuilder(auth)
-            .setPhoneNumber("+91" + binding.inputMobNumber.text.toString())       // Phone number to verify
-            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-            .setActivity(requireActivity())                 // Activity (for callback binding)
-            .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+            .setPhoneNumber("+91 956-958-8740")
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(requireActivity())
+            .setCallbacks(callbacks)
             .build()
-
         binding.signIn.setOnClickListener {
+            binding.progressSignIn.visibility = VISIBLE
             PhoneAuthProvider.verifyPhoneNumber(options)
+        }
+        binding.signUp.setOnClickListener {
+            if (binding.inputName.text.isNullOrBlank()) {
+                firestore.collection("").document(auth.uid!!)
+                    .set(mapOf("name" to binding.inputName.text.toString())).addOnSuccessListener {
+                        navigateToApp()
+                    }
+            }
+
         }
         return binding.root
     }
 
     private fun setupCallbacks() {
         callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-                // This callback will be invoked in two situations:
-                // 1 - Instant verification. In some cases the phone number can be instantly
-                //     verified without needing to send or enter a verification code.
-                // 2 - Auto-retrieval. On some devices Google Play services can automatically
-                //     detect the incoming verification SMS and perform verification without
-                //     user action.
+                binding.progressSignIn.visibility = GONE
                 Log.d(TAG, "onVerificationCompleted:$credential")
                 signInWithPhoneAuthCredential(credential)
             }
 
             override fun onVerificationFailed(e: FirebaseException) {
-                // This callback is invoked in an invalid request for verification is made,
-                // for instance if the the phone number format is not valid.
                 Log.w(TAG, "onVerificationFailed", e)
-
+                binding.progressSignIn.visibility = GONE
                 if (e is FirebaseAuthInvalidCredentialsException) {
-                    // Invalid request
+                    Toast.makeText(requireContext(), "Invalid credentials", Toast.LENGTH_SHORT)
+                        .show()
                 } else if (e is FirebaseTooManyRequestsException) {
-                    // The SMS quota for the project has been exceeded
+                    Toast.makeText(
+                        requireContext(),
+                        "Too many requests! Try again later.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
-
-                // Show a message and update the UI
             }
 
             override fun onCodeSent(
                 verificationId: String,
                 token: PhoneAuthProvider.ForceResendingToken
             ) {
-                // The SMS verification code has been sent to the provided phone number, we
-                // now need to ask the user to enter the code and then construct a credential
-                // by combining the code with a verification ID.
                 Log.d(TAG, "onCodeSent:$verificationId")
-
-                // Save verification ID and resending token so we can use them later
-                val storedVerificationId = verificationId
-                val resendToken = token
+//                val storedVerificationId = verificationId
+//                val resendToken = token
             }
         }
-
-    }
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
@@ -101,14 +100,37 @@ class Auth : Fragment() {
             .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
                     Log.d(TAG, "signInWithCredential:success")
-                    val user = task.result?.user
+                    lifecycleScope.launch {
+                        val userDocument =
+                            firestore.collection("users_collection").document(auth.uid!!).get()
+                                .await()
+                        if (userDocument != null) {
+                            navigateToApp()
+                        } else {
+                            binding.inputName.visibility = VISIBLE
+                            binding.signIn.visibility = GONE
+                            binding.signUp.visibility = VISIBLE
+                        }
+                    }
+
                 } else {
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
                     if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        Toast.makeText(requireContext(), "Invalid credentials", Toast.LENGTH_SHORT)
+                            .show()
                         Log.w(TAG, "signInWithPhoneAuthCredential: ")
                     }
                 }
             }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun navigateToApp()  {
+        Toast.makeText(requireContext(),"Signed In",Toast.LENGTH_SHORT).show()
     }
 
     companion object {
