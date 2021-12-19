d7315adaf64a0b0aa8340d99bdd342aee62e2e4f
DeepanshuPratik <deepanshu.pratik@gmail.com>
2021-12-19 10:07:54 +0530

    Updated Sms Receiver

--- android/app/src/main/java/com/dxn/agventure/MainActivity.kt
+++ android/app/src/main/java/com/dxn/agventure/MainActivity.kt
@@ -1,16 +1,49 @@
 package com.dxn.agventure
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.navigation.fragment.NavHostFragment
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED ){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECEIVE_SMS), 111)
+        }
+        else receiveMsg()
 
+    }
+    override fun onRequestPermissionsResult(requestCode : Int, permissions : Array<out String>, grantResults : IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            receiveMsg()
+    }
+
+    private fun receiveMsg() {
+        var messg = object : BroadcastReceiver(){
+                override fun onReceive(p0: Context?, p1: Intent?){
+                    if(Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT){
+                        for(sms in Telephony.Sms.Intents.getMessagesFromIntent(p1)){
+                            Toast.makeText(applicationContext,sms.displayMessageBody,Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                }
+        }
+        registerReceiver(messg, IntentFilter("android.provider.Telephony.MESSAGE_RECEIVED"))
     }
 }
